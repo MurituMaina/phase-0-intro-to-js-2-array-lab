86f0b5baf4f17ac6c94849639bd9eaad7478325b
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-04 01:25:43 +0300

    Record rogress update

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+const cats = [];
+cats.push('Milo', 'Otis', 'Garfield');
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+
+    cats.push(`${name}`);
+    return cats;
+
+
+}
+console.log(destructivelyAppendCat('Ralph'));
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(`${name}`);
+    return cats;
+}
+console.log(destructivelyPrependCat('Bob'));
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+console.log(destructivelyRemoveLastCat());
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+console.log(destructivelyRemoveFirstCat());
+
+const cat = [];
+
+function prependCat(name) {
+    cat.push([...cats]);
+    cat.unshift(`${name}`);
+    return cat;
+}
+console.log(prependCat("Arnold"));
+
+function removeLastCat() {
+    cat[1].pop();
+    return cat;
+}
+console.log(removeLastCat());
+
+function removeFirstCat() {
+    cat[1].shift();
+    return cat;
+}
+console.log(removeFirstCat());
+console.log(cats);
